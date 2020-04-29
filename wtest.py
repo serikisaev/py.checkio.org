0530a729dcf1ede77fc6ef839117e65ea7787ee8
serik <serikisaev87@gmail.com>
2020-04-30 02:09:09 +0500

    file for testing functions from the job

---
+++ wtest.py
@@ -0,0 +1,22 @@
+# def first_word(text: str) -> str:
+#     count = 0
+#     sublist = []
+#
+#     for i in text:
+#         if ord(i) in range(0, 65) or ord(i) in range(91, 96) or ord(i) in range(123, 127):
+#             if count > 0:
+#                 break
+#             else:
+#                 continue
+#         else:
+#             if ord(i) in range(65, 90) or ord(i) in range(97, 122):
+#                 sublist.append(i)
+#                 count += 1
+#     return ''.join(sublist)
+#
+#
+# if __name__ == '__main__':
+#     print("Example:")
+#     print(first_word("... and so on ..."))
+
+print(ord("'"))
